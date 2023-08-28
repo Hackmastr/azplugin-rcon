a18077231a74ad3c38ae175fef58f2ea79ca1b1b
crashzk <zkservidores@gmail.com>
2023-08-28 20:03:58 -0300

    Add: Translations PT-BR

---
+++ resources/lang/pt_BR/admin.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'title' => 'RCON',
+    'description' => 'Envie comandos RCON para seus servidores.',
+    'run' => 'Enviar',
+
+    // Errors
+    'cantExecuteCommand' => 'Este servidor não consegue executar o comando.',
+    'invalidRconSettings' => 'Configurações RCON inválidas. Verifique se o servidor possui senha/porta RCON configurada.',
+    'rconFailed' => 'Falha ao executar o comando. Por favor, verifique as configurações do servidor.',
+
+    'permissions' => [
+        'rcon' => 'Acesso ao utilitário RCON'
+    ]
+];
